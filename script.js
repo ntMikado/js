c7a8a0ddfb1e0c346958503962347398a21814ee
Jalik <JALIKSHOOTER@gmail.com>
2023-06-22 20:35:54 +0500

    js

---
+++ script.js
@@ -0,0 +1,16 @@
+var userName =prompt('Как вас зовут?')
+var age = prompt('Сколько вам лет')
+var pr1 = +prompt('7-3=?')
+var pr2 = +prompt('5+3=?')
+var pr3 = +prompt('5*7=?')
+var pr4 = +prompt('15/3=?')
+var pr5 = +prompt('37%6=?')
+var sb = +prompt(' Проверьте')
+console.log( 'Ваше имя : '+ userName );
+console.log('Вам : ' +age + ' лет');    
+console.log('Пример 1: 7-3= 4' + " (ваш ответ " + pr1 +")");           
+console.log('Пример 2: 5+3= 2' + " (ваш ответ " + pr2 +")");           
+console.log('Пример 3: 5*7= 35' + " (ваш ответ " + pr3 +")");           
+console.log('Пример 4: 15/3= 5' + " (ваш ответ " + pr4 +")");           
+console.log('Пример 5: 37%6= 1' + " (ваш ответ " + pr5 +")");    
+console.log("Спасибо");      
